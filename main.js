8b5cd358078431174f0bbc56691648c3a55c06d1
Artur Sroka <artursroka@gmail.com>
2020-12-08 19:23:27 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const name = 'Artur';
+const age = 34;
+
+alert(`Siama ${name}, jestem ${name} i wiem, że mamy ${age} lata`);
+console.log('sam ze sobą sie witasz..');
+console.log('to pierwsza oznaka schizofermi');
