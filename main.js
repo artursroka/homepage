43bd98b023f5ff70d391e7cd9fe554a5ee61a42a
Artur Sroka <artursroka@gmail.com>
2021-01-05 21:48:56 +0100

    Add to JS funtion qs

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const age = 34;
 // alert(`Siama ${name}, jestem ${name} i wiem, że mamy ${age} lata`);
 // console.log('sam ze sobą sie witasz..');
 // console.log('to pierwsza oznaka schizofermi');
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = "Artur Sroka - Strona domowa w ramach kursu <br>CO TEN FRONTEND";
